af148fcc532d7aeda1b6d05f985fd135d5d4f3ed
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-06-14 14:06:41 -0500

    cache

--- config.js
+++ config.js
@@ -18,5 +18,14 @@ module.exports = {
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || 'localhost',
         port: process.env.MYSQL_SRV_PORT || 3001,
+    },
+    cacheService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3003,
+    },
+    redis: {
+        host: process.env.REDIS_HOST || 'redis-18818.c85.us-east-1-2.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || 18818,
+        password: process.env.REDIS_PASS || 'rNejGSfDcl2dTDfB8B5UquLTTA4OkXVI',
     }
 }
