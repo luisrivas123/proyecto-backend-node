b8fe2eda7004291503f3c6c4cb037b74146d26b3
luisrivas123 <luis.rivas@correounivalle.edu.co>
2023-05-22 17:17:08 -0500

    documentation

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express');
 
 const config = require('../config.js')
 const user = require('./components/user/network')
 
 const app = express();
+app.use(express.json())
+
+const swaggerDoc = require('./swagger.json');
+
 const port = config.api.port
+
 
 // Router
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api/user', user)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, ()=> {
     console.log(`API listen port: ${port}`);
