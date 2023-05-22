5baa02783b26eaf874fa612f24de739f87fd3c63
luisrivas123 <luis.rivas@correounivalle.edu.co>
2023-05-22 15:15:03 -0500

    controller DB

--- network/response.js
+++ network/response.js
@@ -5,17 +5,17 @@ exports.success = function (req, res, message, status){
     res.status(status).send({
         error: false,
         status: status,
-        body: message,
+        body: statusMessaje,
     })
 }
 
-exports.success = function (req, res, message, status){
+exports.erro = function (req, res, message, status){
     let statusCode = status || 500
     let statusMessaje = message || 'Interanl server error'
 
     res.status(statusCode).send({
         error: false,
         status: status,
-        body: message,
+        body: statusMessaje,
     })
 }
---
+++ store/dummy.js
@@ -0,0 +1,30 @@
+const db = {
+    'user': [
+        { id: 1, name: 'Carlos' },
+        { id: 2, name: 'Luis' },
+    ],
+};
+
+function list(tabla) {
+    return db[tabla];
+}
+
+function get(tabla, id) {
+    let col = list(tabla);
+    return col.filter(item => item.id === id)[0] || null;
+}
+
+function upsert(tabla, data) {
+    db[collection].push(data);
+}
+
+function remove(tabla, id) {
+    return true;
+}
+
+module.exports = {
+    list,
+    get,
+    upsert,
+    remove,
+};
