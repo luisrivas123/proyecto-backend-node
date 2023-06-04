8f554a8805ebc172b625bddfd07269e4a12d9945
luisrivas123 <luis.rivas@correounivalle.edu.co>
2023-06-04 00:41:04 -0500

    verification with token

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,4 +1,6 @@
 const express = require('express')
+
+const secure = require('./secure')
 const response = require('../../../network/response')
 const Controller = require('./index')
 
@@ -10,15 +12,15 @@ router.use(express.json())
 router.get('/', list)
 router.get('/:id', get);
 router.post('/', upsert);
-router.put('/', upsert);
+router.put('/', secure('update'), upsert);
 
 function list(req, res) {
     Controller.list()
         .then((lista) => {
-            response.success(req, res, lista, 200);
+            response.success(req, res, lista, 200)
         })
         .catch((err) => {
-            response.error(req, res, err.message, 500);
+            response.error(req, res, err.message, 500)
         });
     
 }
@@ -26,10 +28,10 @@ function list(req, res) {
 function get(req, res) {
     Controller.get(req.params.id)
         .then((user) => {
-            response.success(req, res, user, 200);
+            response.success(req, res, user, 200)
         })
         .catch((err) => {
-            response.error(req, res, err.message, 500);
+            response.error(req, res, err.message, 500)
         });
     
 }
@@ -37,11 +39,11 @@ function get(req, res) {
 function upsert(req, res) {
     Controller.upsert(req.body)
         .then((user) => {
-            response.success(req, res, user, 201);
+            response.success(req, res, user, 201)
         })
         .catch((err) => {
-            response.error(req, res, err.message, 500);
-        });
+            response.error(req, res, err.message, 500)
+        })
     
 }
 
