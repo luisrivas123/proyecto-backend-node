57565116702b42c26ea495af1de39a5978a334eb
Luis Edgar Rivas <lrivas@truora.com>
2023-06-08 22:58:46 -0500

    DB mysql config

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -14,36 +14,30 @@ router.get('/:id', get);
 router.post('/', upsert);
 router.put('/', secure('update'), upsert);
 
-function list(req, res) {
+function list(req, res, next) {
     Controller.list()
         .then((lista) => {
             response.success(req, res, lista, 200)
         })
-        .catch((err) => {
-            response.error(req, res, err.message, 500)
-        });
+        .catch(next)
     
 }
 
-function get(req, res) {
+function get(req, res, next) {
     Controller.get(req.params.id)
         .then((user) => {
             response.success(req, res, user, 200)
         })
-        .catch((err) => {
-            response.error(req, res, err.message, 500)
-        });
+        .catch(next)
     
 }
 
-function upsert(req, res) {
+function upsert(req, res, next) {
     Controller.upsert(req.body)
         .then((user) => {
             response.success(req, res, user, 201)
         })
-        .catch((err) => {
-            response.error(req, res, err.message, 500)
-        })
+        .catch(next)
     
 }
 
--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'Anthar3s?',
     },
+    mysql: {
+        // host: process.env.MYSQL_HOST || 'localhost:3306',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASS || 'admin123',
+        database: process.env.MYSQL_DB || 'mystore',
+    },
 }
