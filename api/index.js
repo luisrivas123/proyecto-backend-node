5f48ce724a2546ec1f57e2e67fa270169b662f35
luisrivas123 <luis.rivas@correounivalle.edu.co>
2023-06-23 00:26:31 -0500

    list post

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const swaggerUi = require('swagger-ui-express')
 const config = require('../config.js')
 const user = require('./components/user/network')
 const auth = require('./components/auth/network')
+const post = require('./components/post/network')
 const errors = require('../network/errors')
 
 const app = express()
@@ -18,6 +19,7 @@ const port = config.api.port
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api/user', user)
 app.use('/api/auth', auth)
+app.use('/api/post', post)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.use(errors)
