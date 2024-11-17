33121e73134c53a3621041dbb671bb8e3d54146f
luisrivas123 <luis.rivas@correounivalle.edu.co>
2024-11-17 20:37:00 +0100

    update conection mysql

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    api: {
-        port: process.env.API_PORT || 3000
-    },
-    jwt: {
-        secret: process.env.JWT_SECRET || 'Anthar3s?',
-    },
-    mysql: {
-        // host: process.env.MYSQL_HOST || 'localhost:3306',
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASS || 'admin123',
-        database: process.env.MYSQL_DB || 'mystore',
-    },
+  api: {
+    port: process.env.API_PORT || 3000
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'Anthar3s?'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'root',
+    port: process.env.MYSQL_PORT || '3306',
+    password: process.env.MYSQL_PASS || 'password',
+    database: process.env.MYSQL_DB || 'mystore'
+  }
 }
